6016ef8ef49e3cecfd30d7bc5be0a638ea8d337d
Lucio Lima <luciothecomercial@gmail.com>
2025-06-03 00:33:30 -0300

    Mudança no parâmetro geométrico dos widgets e reorganização do código

--- logsafe.py
+++ logsafe.py
@@ -5,14 +5,16 @@ janela.title("LogSafe")
 janela.geometry("980x550")
 janela.configure(bg="#1f1f1f")
 
-label = Label(janela, text="Carteira de Logins", font=("Myriad Pro", 30), bg="black", fg="White")
-label.grid(column=2, row=0)
+label = Label(janela, text="Carteira de Logins", font=("Myriad Pro", 30), bg="#1f1f1f", fg="White")
+label.pack(pady=10)
+
+pin_entry = Entry(janela,width=10,  bg="#383434", fg="white")
+pin_entry.pack(pady=50)
 
 bot = Button(janela, text="Acessar Carteira", bg="black", fg="white", width="20", font=("Myriad Pro", 15))
-bot.grid(column=2, row=3)
+bot.pack(pady=50)
 
-pin_entry = Entry(janela,width=10)
-pin_entry.grid(column=2, row=2)
+
 
 pin_value = pin_entry.get()
 
